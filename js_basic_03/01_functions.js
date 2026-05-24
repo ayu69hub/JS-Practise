d2e38e980ec7c3e1dae66c79dca5ea28326d316a
ayu69hub <singh6268ayush@.com>
2026-05-24 14:18:04 +0530

    lil change

--- js_basic_03/01_functions.js
+++ js_basic_03/01_functions.js
@@ -17,9 +17,9 @@ function sayMyName(){
 
 function addTwoNumbers(number1, number2){
 
-    // let result = number1 + number2
+    let result = number1 + number2
     // return result
-    return number1 + number2
+    // return number1 + number2
 }
 
 const result = addTwoNumbers(3, 5)
